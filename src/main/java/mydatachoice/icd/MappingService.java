7bd3244cacc30f14deb5ff325015411af0fc24c3
HongGuan <hguan6@asu.edu>
2018-01-10 18:37:32 -0700

    fixed the small bug

--- src/main/java/mydatachoice/icd/MappingService.java
+++ src/main/java/mydatachoice/icd/MappingService.java
@@ -154,14 +154,14 @@ public class MappingService {
 
     public Set<String> getCompleteValuesetByRules(Set<String> icd10cm){
         Set<String> completeValueset = new HashSet<>();
-        String pattern = "^(F06|F10|F11|F12|F14|F15|F16|F17|F18|F19|F2|F3|F4|F50|F51|F52|F53|F55|F6|F7|F8|F9|G47|K20|K70|N44.2|N44.8|N50.3|" +
+        String pattern = "^(F06|F1|F2|F3|F4|F50|F51|F52|F53|F55|F6|F7|F8|F9|G47|K20|K70|N44.2|N44.8|N50.3|" +
                 "N50.8|N52.1|N52.9|N53.12|N94.1|N94.8|N94.9|P04.3|P04.4|P04.9|P93.8|R23.2|R37|R78.2|T14.91|T50.99|T51|T74|T76|T40)\\S*";
         for(String code : icd10cm){
             if(code.matches(pattern)){
                 completeValueset.add(code);
             }
         }
-        String exceptionPattern = "^(F28|F51.11|F64.0|G47.2|G47.3|G47.5|T50.996)\\S*";
+        String exceptionPattern = "^(F13|F28|F51.11|F64.0|G47.2|G47.3|G47.5|T50.996)\\S*";
         Iterator<String> itr = completeValueset.iterator();
         while(itr.hasNext()){
             String code = itr.next();
